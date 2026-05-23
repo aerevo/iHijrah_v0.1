1862203076a1e98396499b905c6c7dc4376a369e
aerevo <aerevo554@gmail.com>
2026-05-23 23:59:00 +0800

    Fix formatting issue in daily_card.dart

--- lib/widgets/daily_card.dart
+++ lib/widgets/daily_card.dart
@@ -321,7 +321,7 @@ class _DailyCardShell extends StatelessWidget {
                             ),
                           ),
                         ],
-                      ),
+                      )
                     ),
                   ),
                 ],
